be4acfc4021b18e59c2dcf390d5e00b5e8385dc4
TridipCDas <>
2021-05-31 22:09:03 +0530

    Humanize for unblockable scraping

---
+++ Scraping/humanize.py
@@ -0,0 +1,80 @@
+"""
+Basic start to write unblockable scrapers.
+
+"""
+import requests
+import random
+
+user_agent_list = [
+    # Chrome
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36",
+    # Firefox
+    "Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)",
+    "Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64; Trident/7.0; rv:11.0) like Gecko",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)",
+    "Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; Trident/7.0; rv:11.0) like Gecko",
+    "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)",
+    "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)",
+    "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)",
+    # Postman
+    "PostmanRuntime/7.24.1",
+]
+
+def get_proxies():
+    #NOTE: Depends on the proxy service. Should return a list of proxies.
+    pass
+
+
+def random_params():
+    proxies = get_proxies()
+    if not proxies:
+        proxy = None
+    else:
+        proxy = random.choice(proxies)
+
+
+    user_agent = random.choice(user_agent_list)
+
+    # NOTE: xxxxxxxxx fields are optional.
+    headers = {
+        "Content-Type": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+        "Accept": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+        "Host": "xxxxxxxxxxxxxxxxxxxxxxxx",
+        "Cookie": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+        "Referer": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+        "Content-Type": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+        "Cookie": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+        "User-Agent": user_agent,
+    }
+    #print("Randomizing Payload...")
+
+    return (proxy, headers)
+
+
+#---------------------- HOW TO USE------------------------------
+(proxy, headers) = random_params()
+
+url="http://www..........................."
+
+try:
+    response = requests.post(url,headers=headers,proxies={"http": proxy, "https": proxy})
+except Exception:
+    response = requests.post(url,headers=headers)
+
+
+#-------------------------------------------------------------------
